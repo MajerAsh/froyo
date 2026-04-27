28840cd263b556eca4e8e93fe95104bc299b736f
Ashley Majer <Majerash@gmail.com>
2026-04-27 12:58:02 -0400

    frequency map 2 ways

--- froyo.js
+++ froyo.js
@@ -1,4 +1,11 @@
-/* -------- > test, covert string to lowercase array: 
+/* node froyo.js 
+
+NOTE:
+prompt() function is a Web API that exists only in web browsers. 
+If you are running your code in Node.js, this function does not exist
+
+
+-------- > test, covert string to lowercase array: 
 const str = "jjf uGGo ,   jJJJj ,kk ll   ,    ooo, o";
 
 str.toLowerCase().replaceAll(" ", "").split(",");
@@ -17,14 +24,21 @@ console.log(newStr); // [ 'jjfuggo', 'jjjjj', 'kkll', 'ooo', 'o' ]
     "Check the browser console to see how many of each flavor you ordered.",
   );
 
+  // 4. Parse the user input into an array:
   let flavChoices = input.toLowerCase().replaceAll(" ", "").split(",");
 
-  /* try1:
-  const countEach = flavChoices.reduce((acc, curr) =>
-  (acc !== curr ? 0 : 1), []);*/
+  // 5. Build an object to track which flavors you have counted so far:
+  // With forEach:
+  const countFlavs = {};
 
-  const countEach = flavChoices.reduce((acc, curr) => {
-    acc[curr] = (acc[curr] || 0) + 1;
+  flavChoices.forEach((flav) => {
+    countFlavs[flav] = (countFlavs[flav] || 0) + 1;
+  });
+  console.log(countFlavs);
+
+  // With reduce:
+  const countEach = flavChoices.reduce((acc, flav) => {
+    acc[flav] = (acc[flav] || 0) + 1; // If the fruit exists, increment it in our empty obj {}; otherwise, initialize it to 1
     return acc;
   }, {});
   console.log(countEach);
